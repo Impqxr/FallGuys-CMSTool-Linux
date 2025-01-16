2eacc4ed34535112316e3c3e278ba8fac04680e9
Viktor Fedorov <impqxr@gmail.com>
2025-01-16 15:11:50 +0100

    version improvements

--- flake.nix
+++ flake.nix
@@ -8,18 +8,24 @@
     in
     {
       packages.x86_64-linux.default =
-        pkgs.buildDotnetModule
-          {
-            name = "FallGuys-CMSTool";
+        pkgs.buildDotnetModule rec
+        {
+          # Someone help me to figure out the way to get the version normally or make dotnet see .git directory
+          packageVersion = builtins.readFile (pkgs.runCommand "extract-version" { } ''
+            grep -oP '<Version>\K[^<]+' ${self}/${projectFile} > $out
+          '');
 
-            src = ./.;
+          name = "FallGuys-CMSTool";
+          version = "${packageVersion}+${self.rev or self.dirtyRev}";
 
-            projectFile = "CMSTool/FallGuys-CMSTool.csproj";
-            dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0;
-            dotnet-runtime = pkgs.dotnetCorePackages.runtime_8_0;
-            nugetDeps = ./deps.json;
-            executables = [ "CMSTool" ];
-          };
+          src = ./.;
+
+          projectFile = "CMSTool/FallGuys-CMSTool.csproj";
+          dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0;
+          dotnet-runtime = pkgs.dotnetCorePackages.runtime_8_0;
+          nugetDeps = ./deps.json;
+          executables = [ "CMSTool" ];
+        };
 
       apps.x86_64-linux.default = {
         type = "app";
@@ -29,6 +35,10 @@
       devShells.x86_64-linux.default =
         pkgs.mkShell {
           packages = with pkgs; [ dotnetCorePackages.sdk_8_0 nuget-to-json ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath (with pkgs; [ fontconfig xorg.libX11 xorg.libICE xorg.libSM ])}"
+          '';
         };
     };
 }
